c13dd86a934d5f5e5d6a3470dcbf86dd40cb3637
unknown <yfadza123@gmail.com>
2024-06-14 13:29:41 +0700

    website coffee hopemine

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Toggle class active
+const navbarNav = document.querySelector
+('.navbar-nav');
+//ketika hamburger diklik
+document.querySelector('#hamburger-menu').
+    onclick = () => {
+    navbarNav.classList.toggle('active');
+
+};
+
+// Klik Diluar Sidebar untuk menghilangkan nav
+const hamburger = document.querySelector
+('#hamburger-menu');
+
+document.addEventListener('click', function(e){
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
